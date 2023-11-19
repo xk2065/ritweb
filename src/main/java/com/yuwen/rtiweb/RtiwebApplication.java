e248f093bbd30a60e1b20a92f17fcf23c4ee4641
Yu_wen <a3538772153@163.com>
2023-11-19 12:29:32 +0800

    First Upload

---
+++ src/main/java/com/yuwen/rtiweb/RtiwebApplication.java
@@ -0,0 +1,16 @@
+package com.yuwen.rtiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class RtiwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtiwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuwen/rtiweb/RtiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuwen.rtiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtiwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
